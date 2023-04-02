53421cf3ba60adab7c046b1c1b8114bf6e849ac3
aimen08 <aimen_hamza@hotmail.com>
2023-04-02 02:57:52 +0100

    fix errors

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from src.app import app
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, port=8085)
+    serve(app, port=8085,threads=8)
+    # app.run(debug=True, port=8085)
     
